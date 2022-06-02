e80e11c8fbbfef823ce72ef754a0218dc29c1ed6
dedeanjarpermana <dedeanjarperman@gmail.com>
2022-06-02 16:37:01 +0700

    tugas coding BTS

---
+++ BTS_coding_test/db.js
@@ -0,0 +1,55 @@
+
+const Pool = require ('pg').Pool
+const pool = new Pool({
+    user:"postgres",
+    password:"gadissuci25",
+    database:'shoppingdb',
+    host:"localhost", 
+    port: 5432
+})
+
+
+
+
+// proses add kontak dengan database
+const prosesAddContact =  async (req, res) => {
+    res.render('add_contact',{
+        title: "Add Contact",
+        
+    })
+}
+
+const addContact = async (req, res) => {
+    try {
+        const {rows: insert_contacts_query} = await pool.query(`INSERT INTO user (username, password, email, phone, country, city,postcode, name, address) VALUES ('${req.params.username}', '${req.params.password}', '${req.params.email}', '${req.params.phone}', '${req.params.country}',  '${req.params.city}',  '${req.params.postcode}', '${req.params.name}', '${req.params.address}'    `);
+        add_contacts_query(
+            addContact => 
+            res.render('./add_contact', {
+                title: "Add data ", 
+                addContact
+        })
+        )
+    console.log(insert_contacts_query)
+    }
+    catch (error)
+    {
+        console.error(error.message)
+    }
+};
+        
+const delContact =  async (req, res) => {
+    try {
+        const {rows: del_contacts_query} = await pool.query(`DELETE FROM contacts WHERE name = '${req.params.name}'`)
+        
+    }
+    catch (error)
+    {
+        console.error(error.message)
+    }
+}
+        
+module.exports = {pool,
+                delContact,
+                addContact , 
+                prosesAddContact,
+            }
